17242725b21b9bb97fb1775fb24434e0b1360fe3
guosurui <13012615613@163.com>
2024-04-03 14:36:39 +0800

    feat: 添加通用http响应

---
+++ internal/data/dto/response.go
@@ -0,0 +1,23 @@
+package dto
+
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func NewResponse(code int, message string, data interface{}) *Response {
+	return &Response{
+		Code:    code,
+		Message: message,
+		Data:    data,
+	}
+}
+
+func NewSuccessResponse(data interface{}) *Response {
+	return NewResponse(200, "success", data)
+}
+
+func NewFailResponse(message string) *Response {
+	return NewResponse(500, message, nil)
+}
--- internal/router/user.go
+++ internal/router/user.go
@@ -26,32 +26,31 @@ func (u *UserRouter) HandleRegister(ctx *gin.Context) {
 	// 提取请求体到 Register 结构体
 	body := &dto.Register{}
 	if err := ctx.BindJSON(body); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		ctx.JSON(http.StatusBadRequest, dto.NewFailResponse(err.Error()))
 		return
 	}
 
-	// 由于功能简单，直接调用 domain 层的方法
+	// 由于功能简单，直接调用 domain 的方法
 	user := domain.NewUser()
 	err := user.Register(*body)
 	if err != nil {
 		if err.Error() == "existed user" {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "existed user"})
-			return
+			ctx.JSON(http.StatusBadRequest, dto.NewFailResponse("existed user"))
 		} else {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
+			ctx.JSON(http.StatusInternalServerError, dto.NewFailResponse(err.Error()))
 		}
+		return
 	}
 	// 复杂的话在 service 层处理
 
-	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+	ctx.JSON(http.StatusOK, dto.NewSuccessResponse(nil))
 }
 
 func (u *UserRouter) HandleLogin(ctx *gin.Context) {
 	// 提取请求体到 Register 结构体
 	body := &dto.Login{}
 	if err := ctx.BindJSON(body); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		ctx.JSON(http.StatusBadRequest, dto.NewFailResponse(err.Error()))
 		return
 	}
 
@@ -60,14 +59,14 @@ func (u *UserRouter) HandleLogin(ctx *gin.Context) {
 	err := user.Login(*body)
 	if err != nil {
 		if err.Error() == "wrong password" {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "wrong password"})
+			ctx.JSON(http.StatusBadRequest, dto.NewFailResponse("wrong password"))
 			return
 		} else {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			ctx.JSON(http.StatusInternalServerError, dto.NewFailResponse(err.Error()))
 			return
 		}
 	}
 	// 复杂的话在 service 层处理
 
-	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
+	ctx.JSON(http.StatusOK, dto.NewSuccessResponse(nil))
 }
